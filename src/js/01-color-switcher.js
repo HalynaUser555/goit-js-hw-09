7c5246194ca2fa43599b965c0def57f1faa399dd
Halyna <halyna0193@gmail.com>
2023-07-21 19:12:01 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]')
+};
+const timer = {
+    itervalId: null,
+    start() {
+        this.itervalId = setInterval(() => {
+            const color = getRandomHexColor();
+            document.body.style.background = "" + color;
+        }, 1000);
+    },
+    stop() {
+        clearInterval(this.itervalId);
+        this.itervalId = null;
+    },
+}
+refs.startBtn.addEventListener('click', () => {
+    timer.start();
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+})
+refs.stopBtn.addEventListener('click', () => {
+    timer.stop();
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+}
+)
---
+++ src/js/02-timer.js
@@ -0,0 +1,46 @@
+const button = document.querySelector('[data-start]');
+const daysElem = document.querySelector('[data-days]');
+const hoursElem = document.querySelector('[data-hours]');
+const minutesElem = document.querySelector('[data-minutes]');
+const secondsElem = document.querySelector('[data-seconds]');
+const inputElem = document.querySelector('#datetime-picker');
+
+// button.disabled = true;
+let nIntervId;
+const targetDate = new Date();
+setInterval(() => {
+    const currentDate = new Date();
+    const daltaTime = currentDate - targetDate;
+})
+
+
+
+
+
+
+
+
+
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
